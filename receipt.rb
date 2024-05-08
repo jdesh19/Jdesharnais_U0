098868a13a13ffde2ed81f3666b0adca153abf1a
jdesh19 <jarondesharnais@gmail.com>
2024-05-07 19:48:04 -0500

    Created get_grand_total function

---
+++ receipt.rb
@@ -0,0 +1,22 @@
+# Jaron Desharnais U0
+sub_total = 30
+GST = 5
+PST = 7
+
+def get_grand_total(sub_total)
+    
+
+    pst_dollar_amount = (PST * sub_total) / 100.0
+    gst_dollar_amount = (GST * sub_total) / 100.0
+
+    total_tax_amount = pst_dollar_amount + gst_dollar_amount
+
+    grand_total = sub_total + total_tax_amount
+
+    puts "Subtotal: $#{sub_total}"
+    puts "PST: $#{pst_dollar_amount} - #{PST}%"
+    puts "GST: $#{gst_dollar_amount} - #{GST}%"
+    puts "Grand Total: $#{grand_total}"
+end
+
+get_grand_total(sub_total)
